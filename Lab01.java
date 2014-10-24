21efce8ca79eee091409bde4bd231fcfb9c3d7e6
rileyevans <riley_evans@asl.org>
2014-10-24 09:14:33 +0100

    i did the task
    the robots look lilke they should :)

--- Lab01.java
+++ Lab01.java
@@ -2,8 +2,8 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <Riley Evans> 
+* @version <24/10/2014> 
 */
 
 import edu.fcps.karel2.Display;
@@ -12,6 +12,82 @@ import edu.fcps.karel2.Robot;
 public class Lab01 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
+     Display.openWorld("maps/school.map");
+   Display.setSize(10, 10);
+   Display.setSpeed(3);
+   Robot lisa = new Robot();
+   Robot pete = new Robot(4, 5, Display.SOUTH, 0);
+   lisa.move();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+     lisa.pickBeeper();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.move();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.move();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.move();
+      lisa.putBeeper();
+   lisa.move();
+   pete.move();
+      pete.pickBeeper();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+   pete.turnLeft();
+   pete.move();
+   pete.move();
+      pete.putBeeper();
+   pete.turnLeft();
+   pete.turnLeft();
+   pete.move();
+   lisa.turnLeft();
+   lisa.turnLeft();
+   lisa.move();
+   lisa.turnLeft();
+   lisa.move();
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
      }
 }
